b59a30dfceba600802311009dbb5235796ae6105
PranavTamboli <141416864+PranavTamboli@users.noreply.github.com>
2024-10-23 18:27:23 +0530

    Add files via upload

---
+++ core_java/DataTypes.java
@@ -0,0 +1,26 @@
+package com.java.programs;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int i=5;
+		float f=10.1234f;
+		double d = 10000.12345;
+		char c = 'A';
+		boolean b = true;
+		byte b1 = 20;
+		short s = 30000;
+		long l = 20000000;
+		
+		System.out.println("All data types are : ");
+		System.out.println("Integer: " +i);
+		System.out.println("Float: " +f);
+		System.out.println("Double: " +d);
+		System.out.println("Character: " +c);
+		System.out.println("Boolean: " +b);
+		System.out.println("Byte: " +b1);
+		System.out.println("Short: " +s);
+		System.out.println("Long: " +l);
+		
+	}
+}
